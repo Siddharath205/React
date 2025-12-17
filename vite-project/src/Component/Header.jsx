48c4a246af5e9c83269b15f204a64fe67f822009
Siddharath205 <siddharathmishra3@gmail.com>
2025-12-17 12:11:16 +0530

    refactor: rename Home component to Main and update its content; modify Header for responsive design

--- vite-project/src/Component/Header.jsx
+++ vite-project/src/Component/Header.jsx
@@ -1,23 +1,22 @@
 import React from 'react'
-import { IoMenu } from "react-icons/io5";
-
+import { IoMdMenu } from "react-icons/io";
 function Header() {
   return (
     <div className='bg-[#a3d154] flex justify-between items-center p-4'>
         <img src="/vite.svg" alt=""width={20} height={20} />
-        <ul  className='flex space-x-4'>
+        <ul  className='hidden md:flex space-x-4'>
             <li>home</li>
             <li>About</li>
             <li>Contact</li>
             <li>Blog</li>
         </ul>
-        <div>
+        <div className='hidden md:flex space-x-4'>
             <button className='text-3xl  font-semibold italic text-teal-600 '>Login</button>
             <button>Sign up</button>
         </div>
-        <IoMenu />
+        <IoMdMenu className='w-8 h-8 text-red-500 flex md:hidden '/>
     </div>
   )
 }
 
-export default Header
+export default Header
--- vite-project/src/Component/Home.jsx
+++ vite-project/src/Component/Home.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
 
-function home() {
+function Main() {
   return (
-    <div>this is the Footer Component </div>
+    <>
+    <div className='text-lg sm:text-xl md:text-2xl lg:text-4xl text-fuchsia-500'>This is Main</div>
+    <div className='flex flex-wrap justify-center mx-auto gap-5'>
+   {
+    Array.from({length:5}).map((_,index)=>(
+      <div key={index} className='w-64 bg-linear-to-r from-purple-400 via-pink-500 to-red-500 p-4 rounded-lg space-y-4'>
+        <img src="/vite.svg" className='w-full h-24' alt="" />
+        <p className='text-zinc-800 font-semibold'>Blog of Developer</p>
+        <p className='text-base text-zinc-800'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe numquam earum ipsam unde assumenda, rem voluptatum est dolores voluptates harum!</p>
+      </div>
+    ))}
+    </div>
+   
+    </>
   )
 }
 
-export default home
+export default Main
